85fcade1f12c6214d35d8aabca40f76f59cae8f4
ricardo-nikolay2 <ricardo.nikolay@gmail.com>
2025-04-17 12:49:37 +0700

    menambahkan Button pada MainPage.xaml

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace HybridTodoApp
+﻿using System.Threading.Tasks;
+
+namespace HybridTodoApp
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // Add your button click logic here
+            await DisplayAlert("Button Clicked", "You clicked the button!", "OK");
         }
     }
 }
